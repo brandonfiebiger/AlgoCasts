1733fee490116d1c29bf32b83f074c3aaed9adbd
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-12-09 16:57:46 -0700

    Set up node and tree class, implement traverseBF method on tree.

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,35 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    let checkArray = [this.root];
+
+    while(checkArray.length) {
+      fn(checkArray[0])
+      checkArray = [...checkArray, ...checkArray[0].children];
+      checkArray.shift();
+    }
+  }
+}
 
 module.exports = { Tree, Node };
