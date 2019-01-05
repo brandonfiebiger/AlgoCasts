cda697bb3a73b320bf5102aae4787697cd55d52d
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-05 14:26:53 -0700

    Reimplement recursive pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -36,21 +36,45 @@
 
 
 
-function pyramid(n) {
-  const center = Math.floor((2 * n -1) / 2);
+// function pyramid(n) {
+//   const center = Math.floor((2 * n -1) / 2);
 
-  for (let row = 0; row < n; row++) {
-    let level = '';
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
 
-    for (let column = 0; column < 2 * n -1; column++) {
-      if (center - row <= column && center + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
-    console.log(level);
+//     for (let column = 0; column < 2 * n -1; column++) {
+//       if (center - row <= column && center + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+
+function pyramid(n, row = 0, level = '') {
+
+  if (row === n) {
+    return;
   }
+
+  if (level.length === n * 2 - 1) {
+    console.log(level);
+    return pyramid(n, row + 1, level = '');
+  }
+
+  const center = Math.floor((2 * n - 1) / 2);
+
+  if (center - row <= level.length && center + row >= level.length) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+
+  return pyramid(n, row, level);
+
 }
 
 
