93c5813807d8f14fa0ceada40aa4b16b6cc00925
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-05 20:14:10 -0700

    Reimplement merge sort with comments on the code

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -32,27 +32,39 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  //base case for recursive function
   if (arr.length === 1) {
     return arr;
   }
-
-  const center = Math.floor(arr.length / 2);
+  //find the center of the array
+  const center = Math.floor((arr.length) / 2);
+  //slice off the left side of the array
   const left = arr.slice(0, center);
+  //slice off the right side of the array
   const right = arr.slice(center);
 
+  //return recursive call to merge and merge sort until
+  //each array is broken down to one element 
+  //and recursively sorted back together
   return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  //initialize empty results array
   const results = [];
 
+  //while there is still length in both the left and right arrays
   while (left.length && right.length) {
+    //if the 0 index of the left array is less than the 0 index of the right array
     if (left[0] < right[0]) {
+      //push the 0 index of the left array into results
       results.push(left.shift());
     } else {
+      //otherwise push the 0 index of the right array into the results
       results.push(right.shift());
     }
   }
+  //return and array with the results spread in, followed by what's left of the left and right arrays
   return [...results, ...left, ...right];
 }
 
