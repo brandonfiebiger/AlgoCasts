f596f6c0c777747adf9bd579f0c90a10d88826b9
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-01-06 14:10:02 -0700

    More practice with mergeSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -31,41 +31,102 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {
-  //base case for recursive function
-  if (arr.length === 1) {
-    return arr;
-  }
-  //find the center of the array
-  const center = Math.floor((arr.length) / 2);
-  //slice off the left side of the array
-  const left = arr.slice(0, center);
-  //slice off the right side of the array
-  const right = arr.slice(center);
 
-  //return recursive call to merge and merge sort until
-  //each array is broken down to one element 
-  //and recursively sorted back together
-  return merge(mergeSort(left), mergeSort(right));
-}
+
+
+
+
+
+
 
 function merge(left, right) {
   //initialize empty results array
-  const results = [];
-
-  //while there is still length in both the left and right arrays
+  let results = [];
+  // while there are still elements in the left and right arrays
   while (left.length && right.length) {
-    //if the 0 index of the left array is less than the 0 index of the right array
+    //if the first element of the left array is less than the first element in the right array
     if (left[0] < right[0]) {
-      //push the 0 index of the left array into results
+      // Shift the first element off of the left array and push it into the results array
       results.push(left.shift());
     } else {
-      //otherwise push the 0 index of the right array into the results
+      //otherwise Shift the first element out of the right array and push it into the results array
       results.push(right.shift());
     }
   }
-  //return and array with the results spread in, followed by what's left of the left and right arrays
-  return [...results, ...left, ...right];
+  //at last return a new array spreading in the results array first
+  //then spread in what's left of the right && left arrays, it does not matter which
+  //order because at least one of them has to be empty
+  return [...results, ...right, ...left];
 }
 
+function mergeSort(arr) {
+  //setup base case for recursion
+  // if the length of the array is exactly one, we will return it
+  if (arr.length === 1) {
+    return arr;
+  }
+  //find the center of the array and slice it into two halves to feed
+  //to the merge function
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  //return a recursive call to merge and mergSort
+  //this will break down the array until there is one element and 
+  //return all of those single elements which will all
+  //be joined and sorted by the recursive call to merge being returned
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+
+// function mergeSort(arr) {
+//   //base case for recursive function
+//   if (arr.length === 1) {
+//     return arr;
+//   }
+//   //find the center of the array
+//   const center = Math.floor((arr.length) / 2);
+//   //slice off the left side of the array
+//   const left = arr.slice(0, center);
+//   //slice off the right side of the array
+//   const right = arr.slice(center);
+
+//   //return recursive call to merge and merge sort until
+//   //each array is broken down to one element 
+//   //and recursively sorted back together
+//   return merge(mergeSort(left), mergeSort(right));
+// }
+
+// function merge(left, right) {
+//   //initialize empty results array
+//   const results = [];
+
+//   //while there is still length in both the left and right arrays
+//   while (left.length && right.length) {
+//     //if the 0 index of the left array is less than the 0 index of the right array
+//     if (left[0] < right[0]) {
+//       //push the 0 index of the left array into results
+//       results.push(left.shift());
+//     } else {
+//       //otherwise push the 0 index of the right array into the results
+//       results.push(right.shift());
+//     }
+//   }
+//   //return and array with the results spread in, followed by what's left of the left and right arrays
+//   return [...results, ...left, ...right];
+// }
