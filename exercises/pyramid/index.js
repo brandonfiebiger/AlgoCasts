0a24fe72e1015fd2a365c3b4aff6ab1d31f47e44
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-03-05 16:52:55 -0700

    Add recursive solution for pyramid problem (extra practice).

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -16,23 +16,54 @@
 
 
 
-const pyramid = (n) => {
+const pyramid = (n, row = 0, level = '') => {
+
+  if (row === n) {
+    return;
+  }
 
   const center = Math.floor((2 * n - 1) / 2);
 
-  for (let row = 0; row < n; row++) {
-    let step = "";
-    
-    for (let column = 0; column < (2 * n - 1); column++) {
-      if (column <= row + center && column >= center - row) {
-        step += '#';
-      } else {
-        step += ' ';
-      }
-    }
-    console.log(step);
+  if (level.length === (2 * n - 1)) {
+    console.log(level);
+    return pyramid(n, row + 1, '');
   }
+
+  if (center - row <= level.length && center + row >= level.length) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+
+  pyramid(n, row, level);
 }
+
+
+
+
+
+
+
+
+
+
+// const pyramid = (n) => {
+
+//   const center = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let step = "";
+    
+//     for (let column = 0; column < (2 * n - 1); column++) {
+//       if (column <= row + center && column >= center - row) {
+//         step += '#';
+//       } else {
+//         step += ' ';
+//       }
+//     }
+//     console.log(step);
+//   }
+// }
 
 
 
