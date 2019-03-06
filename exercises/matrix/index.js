1dff9c9806102d048b3069b982954aa7be0ceba2
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-03-06 15:16:26 -0700

    More spiral matrix practice.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -18,52 +18,124 @@
 
 
 
-function matrix(n) {
+// function matrix(n) {
+//   const results = [];
+
+//   for (let i = 0; i < n; i++) {
+//     results.push([]);
+//   }
+
+//   let startColumn = 0;
+//   let startRow = 0;
+//   let endColumn = n - 1;
+//   let endRow = n - 1;
+//   let counter = 1;
+
+
+//   while (startRow <= endRow && startColumn <= endColumn) {
+
+//     //top row
+//     for (let i = startColumn; i <= endColumn; i++) {
+//       results[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+    
+//     //right side
+//     for (let i = startRow; i <= endRow; i++) {
+//       results[i][endColumn] = counter;
+//       counter++;
+//     }
+//     endColumn--;
+    
+//     //bottom row
+//     for (let i = endColumn; i >= startColumn; i--) {
+//       results[endRow][i] = counter;
+//       counter++;
+//     }
+//     endRow--;
+    
+//     //left side
+//     for (let i = endRow; i >= startRow; i--) {
+//       results[i][startColumn] = counter;
+//       counter++;
+//     }
+//     startColumn++;
+//   }
+//   return results;
+// }
+
+
+
+
+
+
+
+
+
+
+const matrix = (n) => {
+
+  //initialize array of arrays
   const results = [];
 
   for (let i = 0; i < n; i++) {
     results.push([]);
   }
-
+  //initailize variables for matrix
+  let counter = 1;
   let startColumn = 0;
   let startRow = 0;
-  let endColumn = n - 1;
   let endRow = n - 1;
-  let counter = 1;
-
-
+  let endColumn = n - 1;
+  
   while (startRow <= endRow && startColumn <= endColumn) {
-
     //top row
     for (let i = startColumn; i <= endColumn; i++) {
       results[startRow][i] = counter;
       counter++;
     }
     startRow++;
-    
-    //right side
-    for (let i = startRow; i <= endRow; i++) {
-      results[i][endColumn] = counter;
-      counter++;
-    }
-    endColumn--;
-    
-    //bottom row
-    for (let i = endColumn; i >= startColumn; i--) {
-      results[endRow][i] = counter;
-      counter++;
-    }
-    endRow--;
-    
-    //left side
-    for (let i = endRow; i >= startRow; i--) {
-      results[i][startColumn] = counter;
-      counter++;
-    }
-    startColumn++;
+
+
+
+      //right side
+      for (let i = startRow; i <= endRow; i++) {
+        results[i][endColumn] = counter;
+        counter++
+      }
+      endColumn--;
+      
+      //bottom row
+      for (let i = endColumn; i >= startColumn; i--) {
+        results[endRow][i] = counter;
+        counter++
+      }
+      endRow--;
+      
+      //right side
+      for (let i = endRow; i >= startRow; i--) {
+        results[i][startColumn] = counter;
+        counter++;
+      }
+      startColumn++;
   }
-  return results;
+    
+    
+    
+    return results;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
