27e2385aead1e7e7d1246d1ccaca73131843369c
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-04-10 12:43:58 -0600

    More matrix practice.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -18,6 +18,65 @@
 
 
 
+const matrix = (n) => {
+
+  const results = [];
+
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  };
+
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  let counter = 1;
+
+  while(startRow <= endRow && startColumn <= endColumn) {
+
+    
+    //top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    
+    //right side
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+    
+    //bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    
+    //left side
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+    
+  }
+    return results;
+}
+
+
+
+
+
+
+
+
+
+
+
 // function matrix(n) {
 //   const results = [];
 
@@ -74,57 +133,57 @@
 
 
 
-const matrix = (n) => {
+// const matrix = (n) => {
 
-  //initialize array of arrays
-  const results = [];
+//   //initialize array of arrays
+//   const results = [];
 
-  for (let i = 0; i < n; i++) {
-    results.push([]);
-  }
-  //initailize variables for matrix
-  let counter = 1;
-  let startColumn = 0;
-  let startRow = 0;
-  let endRow = n - 1;
-  let endColumn = n - 1;
+//   for (let i = 0; i < n; i++) {
+//     results.push([]);
+//   }
+//   //initailize variables for matrix
+//   let counter = 1;
+//   let startColumn = 0;
+//   let startRow = 0;
+//   let endRow = n - 1;
+//   let endColumn = n - 1;
   
-  while (startRow <= endRow && startColumn <= endColumn) {
-    //top row
-    for (let i = startColumn; i <= endColumn; i++) {
-      results[startRow][i] = counter;
-      counter++;
-    }
-    startRow++;
+//   while (startRow <= endRow && startColumn <= endColumn) {
+//     //top row
+//     for (let i = startColumn; i <= endColumn; i++) {
+//       results[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
 
 
 
-      //right side
-      for (let i = startRow; i <= endRow; i++) {
-        results[i][endColumn] = counter;
-        counter++
-      }
-      endColumn--;
+//       //right side
+//       for (let i = startRow; i <= endRow; i++) {
+//         results[i][endColumn] = counter;
+//         counter++
+//       }
+//       endColumn--;
       
-      //bottom row
-      for (let i = endColumn; i >= startColumn; i--) {
-        results[endRow][i] = counter;
-        counter++
-      }
-      endRow--;
+//       //bottom row
+//       for (let i = endColumn; i >= startColumn; i--) {
+//         results[endRow][i] = counter;
+//         counter++
+//       }
+//       endRow--;
       
-      //right side
-      for (let i = endRow; i >= startRow; i--) {
-        results[i][startColumn] = counter;
-        counter++;
-      }
-      startColumn++;
-  }
+//       //right side
+//       for (let i = endRow; i >= startRow; i--) {
+//         results[i][startColumn] = counter;
+//         counter++;
+//       }
+//       startColumn++;
+//   }
     
     
     
-    return results;
-}
+//     return results;
+// }
 
 
 
